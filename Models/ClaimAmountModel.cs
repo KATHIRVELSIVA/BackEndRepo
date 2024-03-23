7cad27869f7bb2e5d8b7c270a293dccaf9dbd180
Kathirvel <kathirvelsivaking@gmail.com>
2024-03-24 00:26:40 +0530

    Upto Getting the database

---
+++ Models/ClaimAmountModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudMicroProject.Models
+{
+    public class ClaimAmountModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public int PolicyID { get; set; }
+        public int AddOnPolicyID { get; set; }
+        public int ClaimID { get; set; }
+        public int IDVvalue { get; set; }
+        public double ClaimAmount { get; set; }
+    }
+}
---
+++ Models/InsuranceApplyModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudMicroProject.Models
+{
+    public class InsuranceApplyModel
+    {
+        [Key]
+        public int ApplyId { get; set; }
+        public int UserID { get; set; }
+        public int PolicyID { get; set; }
+        public int AddOnPolicyID { get; set; }
+    }
+}
